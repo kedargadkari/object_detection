b9dcb647d1921727646e81575d6c5c04dd398f23
Kedar Gadkari <101412314+kedargadkari@users.noreply.github.com>
2024-02-18 19:44:58 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,261 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_vision/flutter_vision.dart';
+
+enum Options { none, imagev5, imagev8, imagev8seg, frame, tesseract, vision }
+
+late List<CameraDescription> cameras;
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late FlutterVision vision;
+  Options option = Options.imagev5;
+  @override
+  void initState() {
+    super.initState();
+    vision = FlutterVision();
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await vision.closeTesseractModel();
+    await vision.closeYoloModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: YoloImageV5(vision: vision),
+    );
+  }
+}
+
+class YoloImageV5 extends StatefulWidget {
+  final FlutterVision vision;
+  const YoloImageV5({Key? key, required this.vision}) : super(key: key);
+
+  @override
+  State<YoloImageV5> createState() => _YoloImageV5State();
+}
+
+class _YoloImageV5State extends State<YoloImageV5> {
+  late List<Map<String, dynamic>> yoloResults;
+  bool isLoaded = false;
+  File? imageFile;
+  int imageHeight = 1;
+  int imageWidth = 1;
+  dynamic camera;
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    loadYoloModel().then((value) {
+      setState(() {
+        yoloResults = [];
+        isLoaded = true;
+      });
+    });
+
+    availableCameras().then((value) {
+      setState(() {
+        final firstCamera = value.first;
+        _controller = CameraController(
+          // Get a specific camera from the list of available cameras.
+          firstCamera,
+          // Define the resolution to use.
+          ResolutionPreset.medium,
+        );
+
+        camera = firstCamera;
+        _initializeControllerFuture = _controller.initialize();
+      });
+    });
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    if (!isLoaded) {
+      return const Scaffold(
+        body: Center(
+          child: Text("Model not loaded, waiting for it"),
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          'Object Recogtion',
+          style: TextStyle(color: Colors.white),
+        )),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          imageFile != null ? Image.file(imageFile!) : const SizedBox(),
+          camera != null && imageFile == null
+              ? FutureBuilder<void>(
+                  future: _initializeControllerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      var scale = size.aspectRatio *
+                          _controller.value.aspectRatio *
+                          1.5;
+                      // If the Future is complete, display the preview.
+                      return Transform.scale(
+                        scale: scale,
+                        child: Center(
+                          child: CameraPreview(_controller),
+                        ),
+                      );
+                    } else {
+                      // Otherwise, display a loading indicator.
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                )
+              : Container(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      // Take the Picture in a try / catch block. If anything goes wrong,
+                      // catch the error.
+                      try {
+                        // Ensure that the camera is initialized.
+                        await _initializeControllerFuture;
+
+                        // Attempt to take a picture and get the file `image`
+                        // where it was saved.
+                        final file = await _controller.takePicture();
+                        setState(() {
+                          imageFile = File(file.path);
+                          yoloOnImage();
+                        });
+                      } catch (e) {
+                        // If an error occurs, log the error to the console.
+                        print(e);
+                      }
+                    },
+                    child: const Text('Take a Photo',)),
+                ElevatedButton(
+                  onPressed: () {
+                    yoloResults.clear();
+                    setState(() {
+                      imageFile = null;
+                    });
+                  },
+                  child: const Text("Clear"),
+                )
+              ],
+            ),
+          ),
+          ...displayBoxesAroundRecognizedObjects(size),
+        ],
+      ),
+    );
+  }
+
+  Future<void> loadYoloModel() async {
+    await widget.vision.loadYoloModel(
+        labels: 'assets/labels.txt',
+        modelPath: 'assets/yolov5n.tflite',
+        modelVersion: "yolov5",
+        quantization: false,
+        numThreads: 2,
+        useGpu: true);
+    setState(() {
+      isLoaded = true;
+    });
+  }
+
+  yoloOnImage() async {
+    yoloResults.clear();
+    Uint8List byte = await imageFile!.readAsBytes();
+    final image = await decodeImageFromList(byte);
+    imageHeight = image.height;
+    imageWidth = image.width;
+    final result = await widget.vision.yoloOnImage(
+        bytesList: byte,
+        imageHeight: image.height,
+        imageWidth: image.width,
+        iouThreshold: 0.8,
+        confThreshold: 0.4,
+        classThreshold: 0.5);
+    if (result.isNotEmpty) {
+      setState(() {
+        yoloResults = result;
+      });
+    }
+  }
+
+  List<Widget> displayBoxesAroundRecognizedObjects(Size screen) {
+    if (yoloResults.isEmpty) return [];
+
+    double factorX = screen.width / (imageWidth);
+    double imgRatio = imageWidth / imageHeight;
+    double newWidth = imageWidth * factorX;
+    double newHeight = newWidth / imgRatio;
+    double factorY = newHeight / (imageHeight);
+
+    double pady = (screen.height - newHeight) / 2;
+
+    Color colorPick = const Color.fromARGB(255, 50, 233, 30);
+    return yoloResults.map((result) {
+      return Positioned(
+        left: result["box"][0] * factorX,
+        top: result["box"][1] * factorY + pady,
+        width: (result["box"][2] - result["box"][0]) * factorX,
+        height: (result["box"][3] - result["box"][1]) * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            border: Border.all(color: Colors.pink, width: 2.0),
+          ),
+          child: Text(
+            "${result['tag']} ${(result['box'][4] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = colorPick,
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+}
